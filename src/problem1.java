cb535815e6a8b7ddd957575106f65f8153782d04
annafongg <78882159+annafongg@users.noreply.github.com>
2021-02-23 00:43:25 -0800

    1

---
+++ src/problem1.java
@@ -0,0 +1,40 @@
+public class problem1 {
+    public static  void main(String[] args) {
+
+        //Create an array that holds 20 positions, give each index a
+        //random value between 1-6, then use a for loop to count
+        //how many 1's were randomly generated in  the array
+
+
+        //hold 20 integers, define the variable (count) to count the number of 1's randomly generated
+        int[] integers = new int[21];
+        int count = 0;
+
+            //count 20 positions, stop after the 20th
+            for(int i = 1; i < 21; i++) {
+                integers[i] =  i * 2;
+                if(i == 21) {
+                   break; }
+
+                //generate random value between 1-6, increase count value by 1 every time random number = 1
+                int rand = (int)((Math.random() * 6) + 1);
+                    if (rand==1) {
+                        count++;
+                    }
+                //print positions + values
+                System.out.println("Value: " + rand +  "\t(Pos: " + i + ")");
+        }
+        //print number of times number 1 was randomly generated
+        System.out.println("\nNumber of times the value 1 was randomly generated: " + count);
+
+
+    }
+}
+
+
+
+
+
+
+
+
